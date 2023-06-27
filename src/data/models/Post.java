cbb90704931c6fb8ae8f6ea2fe7f3fac642a7147
Olawalesegun <olawalesegun0@gmail.com>
2023-06-27 19:58:02 +0100

    Here is the Post class in the models package, it simply contain states and POJOS for post

--- src/data/models/Post.java
+++ src/data/models/Post.java
@@ -1,4 +1,52 @@
 package data.models;
 
+import java.time.LocalDateTime;
+
 public class Post {
+    private int id;
+    private String title;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    private String body;
+    private LocalDateTime dateCreated;
+    private int userId;
 }
