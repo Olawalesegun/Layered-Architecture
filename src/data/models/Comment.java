b3bcf5252399fb45a74e9c7ec0a9713709862e82
Olawalesegun <olawalesegun0@gmail.com>
2023-06-27 20:04:01 +0100

    Here is the Comment class in the models package, it simply contain states and POJOS for comment

--- src/data/models/Comment.java
+++ src/data/models/Comment.java
@@ -1,2 +1,43 @@
-package data.models;public class Comment {
+package data.models;
+
+import java.time.LocalDateTime;
+
+public class Comment {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(int userId) {
+        UserId = userId;
+    }
+
+    public LocalDateTime getDateOfComment() {
+        return dateOfComment;
+    }
+
+    public void setDateOfComment(LocalDateTime dateOfComment) {
+        this.dateOfComment = dateOfComment;
+    }
+
+    private String comment;
+    private int UserId;
+    private LocalDateTime dateOfComment;
 }
