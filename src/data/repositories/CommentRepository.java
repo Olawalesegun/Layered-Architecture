8f458cb29c065ef91f1b08b3ef0f5d7cafe07d30
Olawalesegun <olawalesegun0@gmail.com>
2023-07-04 23:08:43 +0100

    Comment Repository for Persistence Layer of the Blog App

--- src/data/repositories/CommentRepository.java
+++ src/data/repositories/CommentRepository.java
@@ -1,4 +1,13 @@
 package data.repositories;
 
+import data.models.Comment;
+
 public interface CommentRepository {
+
+    Comment save(Comment comment);
+    Comment findById(int id);
+    void delete(Comment comment);
+    void delete(int id);
+    void deleteAll();
+    long count();
 }
---
+++ test/data/repositories/UserRepositoryImplTest.java
@@ -0,0 +1,6 @@
+package data.repositories;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserRepositoryImplTest {
+  
+}
