c14fe75260a074770027595b9d83163210d4d722
Olawalesegun <olawalesegun0@gmail.com>
2023-07-04 23:09:56 +0100

    Post Repo Implement Class which implements the Post Repository

--- src/data/repositories/PostRepositoryImpl.java
+++ src/data/repositories/PostRepositoryImpl.java
@@ -1,2 +1,38 @@
-package data.repositories;public class PostRepositoryImpl {
+package data.repositories;
+
+import data.models.Post;
+
+import java.util.List;
+
+public class PostRepositoryImpl implements PostRepository{
+
+    @Override
+    public List<Post> findAll() {
+        return null;
+    }
+
+    @Override
+    public Post save(Post post) {
+        return null;
+    }
+
+    @Override
+    public Post findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Post post) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
 }
