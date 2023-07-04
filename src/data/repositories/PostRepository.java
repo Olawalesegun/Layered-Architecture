dbaffac32818bd237c182016437e2978a9d164da
Olawalesegun <olawalesegun0@gmail.com>
2023-07-04 23:11:03 +0100

    Post Repository is simply an interface

--- src/data/repositories/PostRepository.java
+++ src/data/repositories/PostRepository.java
@@ -1,2 +1,14 @@
-package data.repositories;public interface PostRepository {
+package data.repositories;
+
+import data.models.Post;
+
+import java.util.List;
+
+public interface PostRepository {
+    List<Post> findAll();
+    Post save(Post post);
+    Post findById(int id);
+    void delete(Post post);
+    void deleteAll();
+    long count();
 }
