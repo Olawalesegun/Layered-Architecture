f94e4f81e806039f58eb9abb39dde7e002d3e479
Olawalesegun <olawalesegun0@gmail.com>
2023-07-04 23:22:10 +0100

    a test case to test for all edge cases in the Persistence Player, while being specific to the User

--- test/data/repositories/UserRepositoryImplTest.java
+++ test/data/repositories/UserRepositoryImplTest.java
@@ -1,6 +1,45 @@
 package data.repositories;
 
-import static org.junit.jupiter.api.Assertions.*;
+import data.models.User;
+import data.repositories.UserRepositoryImpl;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 class UserRepositoryImplTest {
-  
+    UserRepository userRepository = new UserRepositoryImpl();
+    User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+    }
+
+    @Test
+    public void saveOneUser_countIsOneTest() {
+        user.setUserName("");
+        assertNotNull(user);
+        userRepository.save(user);
+        assertEquals(1, userRepository.count());
+    }
+
+    @Test
+    public void saveTwoUser_countIsTwoTest() {
+        User user2 = new User();
+        user.setUserName("Segun");
+        userRepository.save(user);
+        user2.setUserName("Tunde");
+        userRepository.save(user2);
+        assertEquals(2, userRepository.count());
+    }
+
+    @Test
+    public void saveOneUser_findOneTest() {
+        user.setUserName("Damidos");
+        userRepository.save(user);
+        assertEquals(user, userRepository.findById(1));
+    }
 }
