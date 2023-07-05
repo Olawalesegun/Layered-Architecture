3a66bf3c0f8b9c6bbab4bd170b772fb49e634445
Olawalesegun <olawalesegun0@gmail.com>
2023-07-05 22:53:13 +0100

    User service Implements the Userservice interface

--- src/services/UserServiceImpl.java
+++ src/services/UserServiceImpl.java
@@ -1,2 +1,17 @@
-package services;public class UserServiceImpl {
+package services;
+
+public class UserServiceImpl implements UserService{
+
+    @Override
+    public void register(String userName, String password) {
+
+    }
+    @Override
+    public void login(String userName, String password) {
+
+    }
+    @Override
+    public void changePassword(String userName, String oldPassWord, String newPassword) {
+
+    }
 }
