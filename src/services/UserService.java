e7b9a5f1b4e3707a8f2e756e54fbf42d004ab2c8
Olawalesegun <olawalesegun0@gmail.com>
2023-07-05 22:52:07 +0100

    User service ----

--- src/services/UserService.java
+++ src/services/UserService.java
@@ -1,2 +1,7 @@
-package services;public interface UserService {
+package services;
+
+public interface UserService {
+    void register(String userName, String password);
+    void login(String userName, String password);
+    void changePassword(String userName, String oldPassWord, String newPassword);
 }
